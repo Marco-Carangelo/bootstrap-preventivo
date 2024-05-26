ea4f43ae7cdd6592f65a49c8328842a4145b4e19
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-26 11:45:22 +0200

    add: discount code function user data acquisition

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict'
+
+// ****** DISCOUNT CODE ******
+
+//Get the discount field 
+const userDiscount = document.getElementById('user-discount');
+console.log(userDiscount);
+
+//Function for user discount code validation
+userDiscount.addEventListener('keyup', function () {
+
+    //Define an array with all the valid discount code
+    const validDiscount = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+
+    //Transform the value of the discount field into uppercase and save it in a variable
+    const upUserDiscount = userDiscount.value.toUpperCase();
+    console.log(upUserDiscount);
+
+
+}
+)
